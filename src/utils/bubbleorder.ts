d242398969da43d22e4136595e38c819ff8fad3c
santiago <santipachon4@gmail.com>
2021-04-07 19:50:00 -0500

    optimized ordering

--- src/utils/bubbleorder.ts
+++ src/utils/bubbleorder.ts
@@ -1,23 +1,22 @@
 export const ordering = (data: any[]): any[] => {
     let _data = data
-    const length = _data.length
-    for (let indexSecond = 1; indexSecond < length; indexSecond++) {
-    for (let indexFirst = 0; indexFirst < (length - indexSecond); indexFirst++) {
-        const episodeLengthFirst = _data[indexFirst].episode.length
-        const episodeLengthSecond = _data[indexFirst + 1].episode.length
-        const lastEpisodeFirst = _data[indexFirst].episode[episodeLengthFirst -1]
-        const lastEpisodeSecond = _data[indexFirst +1].episode[episodeLengthSecond -1]
+    const ordered = _data.sort((a, b) => {
+        const episodeLengthFirst = a.episode.length
+        const episodeLengthSecond = b.episode.length
+        const lastEpisodeFirst = a.episode[episodeLengthFirst -1]
+        const lastEpisodeSecond = b.episode[episodeLengthSecond -1]
         let nFirst = lastEpisodeFirst.lastIndexOf("/");
         let nSecond = lastEpisodeSecond.lastIndexOf("/");
         let numberFirst = lastEpisodeFirst.substr(nFirst +1, lastEpisodeFirst.lenght);
         let numberSecond = lastEpisodeSecond.substr(nSecond +1, lastEpisodeSecond.lenght);
-        if(Number(numberFirst) < Number(numberSecond)) {
-            let auxiliar = _data[indexFirst]
-            _data[indexFirst] = _data[indexFirst +1]
-            _data[indexFirst +1] =  auxiliar
+        if(Number(numberFirst) > Number(numberSecond)) {
+            return -1
+        } else if(Number(numberFirst) < Number(numberSecond)) {
+            return 1
+        } else {
+            return 0
         }
-
-    }
-    }
-    return _data
+    })
+    
+    return ordered
 }
