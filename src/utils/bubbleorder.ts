4ecb8143ba082b968d425b473a55646707637cd7
santiago <santipachon4@gmail.com>
2021-04-07 19:51:54 -0500

    destructuring

--- src/utils/bubbleorder.ts
+++ src/utils/bubbleorder.ts
@@ -1,12 +1,9 @@
 export const ordering = (data: any[]): any[] => {
     let _data = data
     const ordered = _data.sort((a, b) => {
-        const episodeLengthFirst = a.episode.length
-        const episodeLengthSecond = b.episode.length
-        const lastEpisodeFirst = a.episode[episodeLengthFirst -1]
-        const lastEpisodeSecond = b.episode[episodeLengthSecond -1]
-        let nFirst = lastEpisodeFirst.lastIndexOf("/");
-        let nSecond = lastEpisodeSecond.lastIndexOf("/");
+        const [episodeLengthFirst, episodeLengthSecond] = [a.episode.length, b.episode.length]
+        const [lastEpisodeFirst, lastEpisodeSecond] = [a.episode[episodeLengthFirst -1],b.episode[episodeLengthSecond -1]]
+        let [nFirst, nSecond] = [lastEpisodeFirst.lastIndexOf("/"), lastEpisodeSecond.lastIndexOf("/")];
         let numberFirst = lastEpisodeFirst.substr(nFirst +1, lastEpisodeFirst.lenght);
         let numberSecond = lastEpisodeSecond.substr(nSecond +1, lastEpisodeSecond.lenght);
         if(Number(numberFirst) > Number(numberSecond)) {
